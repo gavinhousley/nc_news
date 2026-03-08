c80f6179b33d878445723c4ec424bc0a037ebf19
gavinhousley <gavinhousley@gmail.com>
2026-03-08 18:04:26 +0000

    add netlify to requirements and adjusted logo rendering to be more React friendly

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,9 @@
 import LoginInfo from "./LoginInfo";
-
+import logo from "../src/assets/north-news-logo.png";
 function Header() {
   return (
     <header>
-      <img
-        className="logo"
-        src="./src/assets/north-news-logo.png"
-        alt="Northcoders News Logo"
-      />
+      <img className="logo" src={logo} alt="Northcoders News Logo" />
       <h1>Northcoders News</h1>
       <LoginInfo />
     </header>
